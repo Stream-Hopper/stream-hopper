38c9d8d4b3f096bf38320bfaed9fb664aea4a8a2
kxc3244 <kevin.chawla@mavs.uta.edu>
2021-04-11 02:42:44 -0500

    Front End

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import UserInfo from './Components/UserInfo/UserInfo'
+
+
+function App() {
+  return (
+
+    <div>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ControlCenter/ControlCenter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function ControlCenter(){
+
+
+    return(
+
+        <div>
+            Kevin Chawla
+        </div>
+
+    )
+}
+
+export default ControlCenter
