3e7d5778dd8bf131c5d3f84a42c0c08594e69d9b
Vitmont <sethjaksik@gmail.com>
2021-04-27 22:20:54 -0500

    fixed gpio class

--- hardware/GPIOAPI.mjs
+++ hardware/GPIOAPI.mjs
@@ -1,11 +1,11 @@
 import util from 'util';
+import child_process from 'child_process';
 const exec = util.promisify(child_process.exec);
 
 class GPIOAPI{
     async toggle(port, mode){
-        await exec(`python gpio.py ${port} ${mode}`);
+        await exec(`python hardware/gpio.py ${port} ${mode}`);
     }
 }
 
 const gpio = new GPIOAPI();
-gpio.toggle(1, 1);
