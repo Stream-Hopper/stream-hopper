0e6245a87a1c00512f7dd612573e5bc57b87b16a
Vitmont <sethjaksik@gmail.com>
2021-04-30 16:32:08 -0500

    we done boys

--- hardware/wemo_actions.mjs
+++ hardware/wemo_actions.mjs
@@ -4,23 +4,28 @@ class WEMOAPI {
 	}
 	set_wemo_on(wemo_name){
 		Wemo.Search(wemo_name, function(err,device){
+            if(device != null) {
 			var wemoSwitch = new Wemo(device.ip, device.port);
 			wemoSwitch.setBinaryState(1, function(err,result){
 				if(err) console.error(err);
 				var state = (result == '1') ? 'on' : 'off';
 				console.log(wemo_name + ' is now ' + state);
 			});
+
+            }
 		});
 	}
 	set_wemo_off(wemo_name){
 		Wemo.Search(wemo_name, function(err,device){
             console.log(device)
+            if(device != null) {
 			var wemoSwitch = new Wemo(device.ip, device.port);
 			wemoSwitch.setBinaryState(0, function(err,result){
 				if(err) console.error(err);
 				var state = (result == '1') ? 'on' : 'off';
 				console.log(wemo_name + ' is now ' + state);
 			});
+            }
 		});
 	}
 	state_of_wemo(wemo_name){
